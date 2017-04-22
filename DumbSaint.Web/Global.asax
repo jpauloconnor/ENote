7c081f5c9143baa7c243ad71353e4fc7a413abba
Paul O'Connor <jamespauloconnor@gmail.com>
2017-04-22 10:21:53 -0400

    Initial commit

---
+++ DumbSaint.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DumbSaint.Web.MvcApplication" Language="C#" %>
---
+++ DumbSaint.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DumbSaint.Web.Startup))]
+namespace DumbSaint.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
